ea870e3c5fd4efce626eb3760cf6e858aab895af
maximelt <21maximax@gmail.com>
2018-11-15 00:14:42 +0100

    Modif exo service

--- README.md
+++ README.md
@@ -1,3 +1,3 @@
 # Maxime_Le_Texier_epsi_B2_js
 
-Exercice service : test sur navigateur en localhost:8080/service, après execution de server.js sur nodejs avec la commande: node server.js
+Exercice service : test sur navigateur (localhost:8080/service?type=iso ou localhost:8080/service?type=date ou autre), après execution de server.js sur nodejs avec la commande: node server.js
--- nodejs/server.js
+++ nodejs/server.js
@@ -1,11 +1,16 @@
 var express = require('express');
 var app = express();
-var d = new Date();
-var date = {year:d.getFullYear(), month:d.getMonth(), day:d.getDay()};
-var dateString = JSON.stringify(date);
+var d = new Date;
 
 app.get('/service', function (req, res) {
-  res.send(dateString);
+  if (req.query.type == "iso") {
+    res.send(d.toISOString());
+  } else if (req.query.type == "date") {
+    res.send(d.toDateString());
+  } else {
+    res.send("erreur 404: Page non-existante, Ce chemin n'a pas été configuré");
+  }
+
 });
 
 app.listen(8080, function(){
